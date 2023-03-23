001e8cd9578efb432b2e74ab739c423af28fbe06
yo mama <school@users-MacBook-Pro.local>
2023-03-23 15:49:55 -0500

    added print statement to code

--- H5/src/h5/H5.java
+++ H5/src/h5/H5.java
@@ -16,6 +16,8 @@ public class H5 {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        System.out.println("changed shit");
+        
         
         
     }
